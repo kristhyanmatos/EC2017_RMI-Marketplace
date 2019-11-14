13eb11053620df311927b93f71fc1850e4ac319e
kristhyanmatos <kristhyandematosmaia@gmail.com>
2019-11-14 18:39:46 -0300

    teste de existências de projetos

---
+++ Kristhyan/ABCMarketplace/src/ABCsrc/Client.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ABCsrc;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level; 
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kristhyanmatos
+ */
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Registry miRegistry = LocateRegistry.getRegistry("127.0.0.1", 1234);
+            InterfaceABC stub = (InterfaceABC) miRegistry.lookup("marketplaceABC");
+            
+            System.out.println("olá, está indo tudo muito bem!");
+        } catch (RemoteException | NotBoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
