618c1f93515eac73e4fc17318904be84aed06708
kristhyanmatos <kristhyandematosmaia@gmail.com>
2019-11-20 17:50:33 -0300

    atualização de repositório

--- example/InterfaceRMI.java
+++
@@ -1,8 +0,0 @@
-//Dependências
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface InterfaceRMI extends Remote{
-    //Estruta de chama de método em inteface.java
-    public int add(int x, int y) throws RemoteException;
-}
